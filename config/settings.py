a0f355ae7070bb37e76554133089e613bb061183
raserr11 <tuboemh@gmail.com>
2024-10-04 14:21:42 +0200

    Removing database (not used)

--- config/settings.py
+++ config/settings.py
@@ -59,10 +59,6 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
 }
 
 
